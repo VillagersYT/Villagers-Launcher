be315e1a8a962bef4b1a39c0c5c366f1f870159b
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2023-12-12 19:52:26 +0100

    🔩 Add Crack connexion and account fixes
    You can now log-in with a crack account, is that great ?

--- src/assets/js/panels/login.js
+++ src/assets/js/panels/login.js
@@ -190,7 +190,7 @@ class Login {
         let loginBtn = document.querySelector(".login-btn")
         let mojangBtn = document.querySelector('.mojang')
 
-        mojangBtn.innerHTML = "Offline"
+        mojangBtn.innerHTML = "Compte Crack"
 
         mojangBtn.addEventListener("click", () => {
             document.querySelector(".login-card").style.display = "none";
